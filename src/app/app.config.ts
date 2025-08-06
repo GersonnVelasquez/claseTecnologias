98130a2981b6dff6cf85f1d76368b2eb83cb08c9
Gersonn Velasquez <31450496+GersonnVelasquez@users.noreply.github.com>
2025-08-05 21:02:40 -0600

    ejemplo clase (#1)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyBqosWzR04xGfoFgaj9IDNLqMAVNEkhI98',
+        authDomain: 'clasetecnologias.firebaseapp.com',
+        projectId: 'clasetecnologias',
+        storageBucket: 'clasetecnologias.firebasestorage.app',
+        messagingSenderId: '660994001460',
+        appId: '1:660994001460:web:b49b243287008fbb6c5861',
+        measurementId: 'G-WJLV1WPG0D',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- src/app/components/componente1/componente1.ts
+++ src/app/components/componente1/componente1.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Persona } from '../../servicios/persona';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-componente1-test',
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './componente1-test.html',
   styleUrl: './componente1.scss',
 })
-export class Componente1 {}
+export class Componente1 {
+  personaService = inject(Persona);
+  personas = [];
+  variable1 = 'Hola Mundo';
+
+
+  constructor() {
+    this.getPersonas();
+  }
+
+  agregarPersona() {
+    this.personaService.addPersona({
+      nombre: 'Nuevo Usuario',
+      edad: 30,
+      email: 'gerson@unitec.edu',
+      telefono: '1234567890',
+    });
+  }
+
+  getPersonas() {
+    this.personaService.getPersonas().subscribe((data: any) => {
+      this.personas = data;
+    });
+  }
+}
