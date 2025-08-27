4d6bcac625078d54f783e2a868d382516a266491
Gersonn Velasquez <31450496+GersonnVelasquez@users.noreply.github.com>
2025-08-26 20:52:01 -0600

    agregando login (#6)

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Autenticacion } from '../../servicios/autenticacion';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  formBuilder = inject(FormBuilder);
+  authService = inject(Autenticacion);
 
+  loginForm = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  });
+  registerForm = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  });
+
+  iniciarSesion() {
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    if (email && password) {
+      this.authService
+        .iniciarSesion(email, password)
+        .then((credencial) => {
+          alert('Usuario logueado: ' + credencial.user?.email);
+        })
+        .catch((error) => {
+          alert('Error al iniciar sesión: ' + error.message);
+        });
+    } else {
+      alert('Por favor ingresa un correo y una contraseña');
+    }
+  }
+
+  registrar() {
+    const email = this.registerForm.value.email;
+    const password = this.registerForm.value.password;
+
+    if (email && password) {
+      this.authService
+        .registrarUsuario(email, password)
+        .then((credencial) => {
+          alert('Usuario registrado: ' + credencial.user?.email);
+        })
+        .catch((error) => {
+          alert('Error al registrar: ' + error.message);
+        });
+    } else {
+      alert('Por favor ingresa un correo y una contraseña');
+    }
+  }
+
+  cerrarSesion() {
+    this.authService
+      .cerrarSesion()
+      .then(() => {
+        alert('Sesión cerrada');
+      })
+      .catch((error) => {
+        alert('Error al cerrar sesión: ' + error.message);
+      });
+  }
 }
